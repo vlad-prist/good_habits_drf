4284f277b563c2ae6c2682a75ea3434361f3b77a
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-08-25 12:46:03 +0300

    Изменение контроллера модели Habit

--- habits/urls.py
+++ habits/urls.py
@@ -1,18 +1,34 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from habits.views import HabitViewSet, HabitPublicListAPIView, HabitOwnListAPIView
 
 from habits.apps import HabitsConfig
-from habits.views import (HabitCreateAPIView, HabitDestroyAPIView,
-                          HabitListAPIView, HabitRetrieveAPIView,
-                          HabitUpdateAPIView, HabitPublicListAPIView)
+
 
 app_name = HabitsConfig.name
 
+router = DefaultRouter()
+router.register(r'habit', HabitViewSet, basename='habit')
+
+habit_create = HabitViewSet.as_view({
+    'post': 'create'
+})
+habit_detail = HabitViewSet.as_view({
+    'get': 'retrieve'
+})
+habit_update = HabitViewSet.as_view({
+    'put': 'update',
+    'patch': 'partial_update'
+})
+user_delete = HabitViewSet.as_view({
+    'delete': 'destroy'
+})
 
 urlpatterns = [
-    path('habit/', HabitListAPIView.as_view(), name='habits_list'),
+    path('habit/', HabitOwnListAPIView.as_view(), name='habits_list'),
     path('public/', HabitPublicListAPIView.as_view(), name='public_habits_list'),
-    path('habit/create/', HabitCreateAPIView.as_view(), name='habit_create'),
-    path('habit/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit_detail'),
-    path('habit/<int:pk>/update/', HabitUpdateAPIView.as_view(), name='habit_update'),
-    path('habit/<int:pk>/delete', HabitDestroyAPIView.as_view(), name='habit_destroy'),
-]
+    path('habit/create/', habit_create, name='habit_create'),
+    path('habit/<int:pk>/', habit_detail, name='habit_detail'),
+    path('habit/<int:pk>/update/', habit_update, name='habit_update'),
+    path('habit/<int:pk>/delete', user_delete, name='habit_destroy'),
+]+ router.urls
--- habits/views.py
+++ habits/views.py
@@ -1,60 +1,46 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 
 from habits.models import Habit
 from habits.paginators import HabitPaginator
 from habits.serializers import HabitSerializer
 from users.permissions import IsOwner
+from habits.tasks import add
 
 
-class HabitCreateAPIView(generics.CreateAPIView):
+class HabitViewSet(viewsets.ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    pagination_class = HabitPaginator
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def perform_create(self, serializer):
         habit = serializer.save()
         habit.user_habit = self.request.user
         habit.save()
+        add.delay()
 
 
-class HabitListAPIView(generics.ListAPIView):
-    ''' Список всех привычек авторизованного пользователя '''
-    queryset = Habit.objects.filter()
+@method_decorator(
+    name='list', decorator=swagger_auto_schema(operation_description="Список личных привычек")
+)
+class HabitOwnListAPIView(generics.ListAPIView):
+    ''' Список личных привычек авторизованного пользователя '''
+    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
-    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         queryset = self.queryset.filter(user_habit=self.request.user)
         return queryset
 
 
-@method_decorator(name='list', decorator=swagger_auto_schema(
-    operation_description="Список публичных привычек"
-))
+@method_decorator(
+    name='list', decorator=swagger_auto_schema(operation_description="Список публичных привычек")
+)
 class HabitPublicListAPIView(generics.ListAPIView):
     ''' Список публичных привычек '''
     queryset = Habit.objects.filter(is_public=True)
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
-
-
-class HabitRetrieveAPIView(generics.RetrieveAPIView):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
-    permission_classes = (IsOwner,)
-
-
-class HabitUpdateAPIView(generics.UpdateAPIView):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
-    permission_classes = (IsOwner,)
-
-
-class HabitDestroyAPIView(generics.DestroyAPIView):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
-    permission_classes = (IsOwner,)
-
