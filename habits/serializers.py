8120fe33d62cb37cc9782e0ec5e732067eb3be9b
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-08-27 13:03:13 +0300

    Добавлена новая валидация (Нельзя выполнять привычку реже 1 раза в неделю)

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-
+from rest_framework.serializers import ValidationError
 from habits.models import Habit
 from habits.validators import (DurationSecondsValidator,
                                PleasedRewardHabitValidator,
                                RelatedPleasedHabitValidator,
-                               RelatedRewardHabitValidator)
+                               RelatedRewardHabitValidator, FrequencyValidator)
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -16,4 +16,7 @@ class HabitSerializer(serializers.ModelSerializer):
             RelatedRewardHabitValidator(field1='related_habit', field2='reward'),
             PleasedRewardHabitValidator(field='is_pleasant'),
             DurationSecondsValidator(field='duration'),
+            FrequencyValidator(field='frequency')
         ]
+
+
