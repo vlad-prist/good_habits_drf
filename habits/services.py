f336a846dd103faacaf951dd2756d45a6d8028c1
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-08-27 14:24:33 +0300

    Настройка задач по отправке напоминаний через телеграм бот

---
+++ habits/services.py
@@ -0,0 +1,14 @@
+import requests
+from django.utils.timezone import localtime
+from rest_framework import status
+
+from config import settings
+
+
+def send_telegram_message(habit):
+    message = f"Я буду {habit.action_habit} в {habit.time_habit} в {habit.place_habit}."
+    chat_id = habit.user_habit.tg_chat_id
+    params = {"text": message, "chat_id": chat_id}
+    response = requests.get(f'{settings.TELEGRAM_URL}{settings.TELEGRAM_BOT_TOKEN}/sendMessage', params=params)
+    if response.status_code != status.HTTP_200_OK:
+        print(f"Ошибка при отправке сообщения в Telegram: {response.text}")
---
+++ habits/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+from django.utils.timezone import localtime
+
+from habits.models import Habit
+from habits.services import send_telegram_message
+
+
+@shared_task
+def send_habit_reminder():
+    time_now = localtime()
+    formatted_time = time_now.strftime("%H:%M")
+    print(f'Текущее время {formatted_time}')
+    habits = Habit.objects.filter(time_habit=formatted_time)
+    print(f"Найдено привычек: {habits.count()}")
+    for habit in habits:
+        if habit.user_habit.tg_chat_id:
+            send_telegram_message(habit)
+        else:
+            print(f'{habit.user_habit} не имеет привязки к telegram')
+
